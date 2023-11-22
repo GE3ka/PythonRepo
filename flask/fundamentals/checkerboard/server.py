22eb9778023ca28686398ace29a0e2e31e229bfa
GE3ka <hela_jbeli@hotmail.com>
2023-11-22 22:57:19 +0200

    checkerboard

--- flask/fundamentals/checkerboard/server.py
+++ flask/fundamentals/checkerboard/server.py
@@ -1,9 +1,9 @@
 from flask import Flask  , render_template 
 app = Flask(__name__)  
 
-@app.route('/checkerboard/<int:x>/<int:y>')
-def play(x,y):         
-    return  render_template("index.html",rows=x,columns=y)
+@app.route('/checkerboard/<int:x>/<int:y>/<string:color1>/<string:color2>')
+def play(x,y,color1,color2):         
+    return  render_template("index.html",rows=x,columns=y,color_1=color1,color_2=color2)
 
 if __name__=="__main__":   
     app.run(debug=True)    
