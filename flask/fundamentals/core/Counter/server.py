35947e50ea072af37a52f25b459ad88f29511d5b
GE3ka <hela_jbeli@hotmail.com>
2023-11-24 23:39:39 +0200

    counter

---
+++ flask/fundamentals/core/Counter/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, session, redirect,session,request,redirect
+app = Flask(__name__)
+app.secret_key = 'this is a key'
+
+@app.route('/')
+
+def home():
+    if 'times' not in session:
+        session['times']=0
+    session['times'] += 1    
+    
+    return render_template('index.html',count=session['times'])
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
+
+if __name__ ==('__main__'):
+    app.run(debug=True)
